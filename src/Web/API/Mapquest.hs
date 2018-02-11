324219a4475f134e2c0efa68d1fe35b7ba589a50
ocramz <zocca.marco@gmail.com>
2018-02-11 23:34:57 +0100

    add modules

---
+++ src/Web/API/Mapquest.hs
@@ -0,0 +1,6 @@
+module Web.API.Mapquest where
+
+
+
+
+
---
+++ src/Web/API/Mapquest/Geocoding.hs
@@ -0,0 +1,5 @@
+module Web.API.Mapquest.Geocoding where
+
+-- https://developer.mapquest.com/documentation/geocoding-api/address/get/
+
+-- http://www.mapquestapi.com/geocoding/v1/address
