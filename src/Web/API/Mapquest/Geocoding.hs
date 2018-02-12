e3a8c00e09e13b2daebb2713b14ca88d5f0a0e88
Marco Zocca <marco.zocca@recordunion.com>
2018-02-12 09:41:27 +0100

    add geocoding API request

--- src/Web/API/Mapquest/Geocoding.hs
+++ src/Web/API/Mapquest/Geocoding.hs
@@ -1,7 +1,24 @@
+{-# language OverloadedStrings, DataKinds #-}
 module Web.API.Mapquest.Geocoding where
 
 import Network.HTTP.Req
+import qualified Data.Text as T
+
+import Control.Monad.Catch
+
+import Data.Aeson
 
 -- https://developer.mapquest.com/documentation/geocoding-api/address/get/
 
--- http://www.mapquestapi.com/geocoding/v1/address
+apiRootPath :: Url 'Http
+apiRootPath = http "www.mapquestapi.com" /: "geocoding" /: "v1" /: "address"
+
+
+-- example request :
+-- GET http://www.mapquestapi.com/geocoding/v1/address?key=KEY&location=Washington,DC
+
+instance MonadHttp IO where
+  handleHttpException = throwM
+
+request :: Option 'Http -> IO (JsonResponse a0)
+request = req GET apiRootPath NoReqBody jsonResponse
