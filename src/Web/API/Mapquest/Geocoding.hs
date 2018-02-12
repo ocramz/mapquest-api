4130229ec1f0d843dab1185c55963d0e9144d561
Marco Zocca <marco.zocca@recordunion.com>
2018-02-12 09:09:47 +0100

    add deps

--- src/Web/API/Mapquest/Geocoding.hs
+++ src/Web/API/Mapquest/Geocoding.hs
@@ -1,5 +1,7 @@
 module Web.API.Mapquest.Geocoding where
 
+import Network.HTTP.Req
+
 -- https://developer.mapquest.com/documentation/geocoding-api/address/get/
 
 -- http://www.mapquestapi.com/geocoding/v1/address
