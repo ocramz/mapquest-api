d5bb8152cd717fd38dfc004e4d206edfa781e121
Marco Zocca <marco.zocca@recordunion.com>
2018-02-12 10:44:03 +0100

    asdf

--- src/Web/API/Mapquest/Geocoding.hs
+++ src/Web/API/Mapquest/Geocoding.hs
@@ -1,6 +1,8 @@
 {-# language OverloadedStrings, DataKinds #-}
 module Web.API.Mapquest.Geocoding where
 
+import Data.Monoid
+
 import Network.HTTP.Req
 import qualified Data.Text as T
 
@@ -20,5 +22,23 @@ apiRootPath = http "www.mapquestapi.com" /: "geocoding" /: "v1" /: "address"
 instance MonadHttp IO where
   handleHttpException = throwM
 
-request :: Option 'Http -> IO (JsonResponse a0)
-request = req GET apiRootPath NoReqBody jsonResponse
+request :: FromJSON a => T.Text -> Option 'Http -> IO a
+request apikey opts = do
+  r <- req GET apiRootPath NoReqBody jsonResponse opts'
+  return $ responseBody r where
+    opts' = opts <> ("key" =: apikey) <> ("outFormat" =: ("json" :: T.Text))
+
+-- data ApiOptions = ApiOptions {
+--     aoLocation :: T.Text
+--                              } deriving (Eq, Show)
+
+data GeocodingQuery = GeocodingQuery {
+    gqCity :: T.Text
+  , gqCountry :: T.Text
+                                 } deriving (Eq, Show)
+
+
+options :: Foldable t => t (T.Text, T.Text) -> Option 'Http
+options = foldr (\(k, v) acc  -> (k =: v) <> acc ) mempty
+
+
